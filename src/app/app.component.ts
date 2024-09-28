94de2ff1ca8dab4023a8ed063f892d2c5861d765
Esteban Vilches <Esteban.vilchesf@gmail.com>
2024-09-28 06:17:38 -0300

    FEAT(): primer esquema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CurrencyPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'salesApp';
+
+
+  productsSig = signal<Product[]>([
+    {
+      id:'1',
+      name:'Mote con huesillo',
+      description:'Vaso de 500 ml de mote con huesillo',
+      price: 2000
+    },
+    {
+      id:'2',
+      name:'Jugo de huesillo',
+      description:'Vaso de 500 ml de jugo de huesillo',
+      price: 1500
+    },
+  ])
+
+  PaymentsMethodsSig = signal<PaymentsMethods[]>([
+    {
+      id:'1',
+      name:'Efectivo',
+    },
+    {
+      id:'2',
+      name:'Transferencia',
+    },
+    {
+      id:'3',
+      name:'Tarjeta',
+    },
+  ])
+}
+
+
+
+export interface Product{
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
+export interface PaymentsMethods{
+  id: string;
+  name: string;
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import localeEsCL from '@angular/common/locales/es-CL';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsCL)
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es-CL' },
+
+  ]
 };
