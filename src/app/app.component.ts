4151eb93f52555a248f717af4751d5ee922ff150
Esteban Vilches <Esteban.vilchesf@gmail.com>
2024-09-28 09:16:43 -0300

    FEAT(): se termina confirmación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, computed, effect, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ProductComponent } from "./components/product/product.component";
 import { v4 as uuidv4 } from 'uuid';
+
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,7 +13,7 @@ import { v4 as uuidv4 } from 'uuid';
     CurrencyPipe,
     ProductComponent,
     NgClass
-],
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -22,34 +24,34 @@ export class AppComponent {
 
   productsSig = signal<Product[]>([
     {
-      id:'1',
-      name:'Mote con huesillo',
-      description:'Vaso de 500 ml de mote con huesillo',
+      id: '1',
+      name: 'Mote con huesillo',
+      description: 'Vaso de 500 ml de mote con huesillo',
       price: 2000
     },
     {
-      id:'2',
-      name:'Jugo de huesillo',
-      description:'Vaso de 500 ml de jugo de huesillo',
+      id: '2',
+      name: 'Jugo de huesillo',
+      description: 'Vaso de 500 ml de jugo de huesillo',
       price: 1500
     },
   ])
 
   paymentsMethodsSig = signal<PaymentMethod[]>([
     {
-      id:'1',
-      name:'Efectivo',
-      icon:'bi-currency-dollar'
+      id: '1',
+      name: 'Efectivo',
+      icon: 'bi-currency-dollar'
     },
     {
-      id:'2',
-      name:'Transferencia',
-      icon:'bi-bank'
+      id: '2',
+      name: 'Transferencia',
+      icon: 'bi-bank'
     },
     {
-      id:'3',
-      name:'Tarjeta',
-      icon:'bi-credit-card'
+      id: '3',
+      name: 'Tarjeta',
+      icon: 'bi-credit-card'
     },
   ])
 
@@ -61,7 +63,7 @@ export class AppComponent {
   })
 
 
-  saleSig = computed<Sale>(()=>{
+  saleSig = computed<Sale>(() => {
     const sale: Sale = {
       cart: this.cartSig(),
       date: new Date(),
@@ -73,26 +75,26 @@ export class AppComponent {
 
   salesSig = signal<Sale[]>([]);
 
-  constructor(){
+  constructor() {
     const sales = localStorage.getItem('sales') ?? '';
-    if(sales)
+    if (sales)
       this.salesSig.set(JSON.parse(sales))
-    effect(()=> {
+    effect(() => {
       localStorage.setItem('sales', JSON.stringify(this.salesSig()))
     })
   }
 
 
-  addProductToCart(product: Product){
-    this.cartSig.update(cart =>{
+  addProductToCart(product: Product) {
+    this.cartSig.update(cart => {
 
-      const newCart = {...cart}
+      const newCart = { ...cart }
       let newTotal = 0;
 
       newCart.products.push(product),
 
 
-      newCart.products.forEach(product => newTotal += product.price)
+        newCart.products.forEach(product => newTotal += product.price)
       newCart.total = newTotal;
 
       return newCart
@@ -100,14 +102,14 @@ export class AppComponent {
     })
   }
 
-  removeProductToCart(product: Product){
-    this.cartSig.update(cart =>{
+  removeProductToCart(product: Product) {
+    this.cartSig.update(cart => {
 
-      const newCart = {...cart}
+      const newCart = { ...cart }
       let newTotal = 0;
 
       const index = newCart.products.findIndex(oldProduct => product.id === oldProduct.id);
-      if(index !== -1)
+      if (index !== -1)
         newCart.products.splice(index, 1)
 
 
@@ -120,48 +122,82 @@ export class AppComponent {
     })
   }
 
-  selectPaymentMethod(paymentMethod:PaymentMethod){
+  selectPaymentMethod(paymentMethod: PaymentMethod) {
     this.paymentMethodSelectedSig.set(paymentMethod)
   }
 
-  confirmPayment(){
-    this.salesSig.update(sales=> {
+
+  initPayment() {
+    Swal.fire({
+      title: "¿Deseas confirmar el pago?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirmar",
+      showClass: {
+        popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `
+      },
+      hideClass: {
+        popup: `
+          animate__animated
+          animate__fadeOutDown
+          animate__faster
+        `
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Pago Exitoso!",
+          text: "se ha completado la venta.",
+          icon: "success"
+        }).then(()=>this.confirmPayment());
+
+      }
+    });
+  }
+
+  confirmPayment() {
+    this.salesSig.update(sales => {
       const newSales = [...sales]
 
       newSales.push(this.saleSig())
       return newSales;
     })
-    setTimeout(() => {
-      this.router.navigate([this.router.url]).then(() => {
-        window.location.reload();
-      });
-    }, 500);
+    this.router.navigate([this.router.url]).then(() => {
+      window.location.reload();
+    });
+
   }
 }
 
 
 
-export interface Product{
+export interface Product {
   id: string;
-  uuid?:string;
+  uuid?: string;
   name: string;
   description: string;
   price: number;
 }
 
-export interface PaymentMethod{
+export interface PaymentMethod {
   id: string;
   name: string;
   icon: string;
 
 }
 
-export interface Cart{
+export interface Cart {
 
   products: Product[]
   total: number;
 }
-export interface Sale{
+export interface Sale {
   date: Date,
   cart: Cart
   paymentMethod: PaymentMethod,
